1a74934016c5e57c014eaa6c75964153b062c562
Camille Scott <camille.scott.w@gmail.com>
2015-03-11 17:05:34 -0400

    Add update config function, fixes to parser

--- parse_avida_cfg.py
+++ parse_avida_cfg.py
@@ -1,20 +1,36 @@
 import pandas as pd
+import sys
 
 def parse_avida_cfg(filename):
+    '''
+    Parse an avida config file in avida format
+    and return it in a configkey:configvalue Series
+    '''
     cfg_keys = []
     cfg_values = []
     with open(filename, 'r') as fp:
-        for line in fp:
-            if line.strip().startswith('#') or not line:
+        for n, line in enumerate(fp):
+            if line.strip().startswith('#') or not line.strip():
                 continue
             else:
                 data = line.strip().split()
                 try:
-                    cfg_keys.append(data[0])
-                    cfg_values.append(data[1])
+                    cfg_keys.append(data[0].strip())
                 except IndexError:
-                    pass
-    return pd.Series(values, index=keys)
+                    print >>sys.stderr, 'Warning: Parsing error at line {}. Failure likely imminent'.format(n)
+                if len(data) > 1 and not data[1].strip().startswith('#'):
+                    cfg_values.append(data[1].strip())
+                else:
+                    cfg_values.append('')
+
+    return pd.Series(cfg_values, index=cfg_keys)
+
+def cfg_series_to_avida_fmt(cfg_series, fp):
+    '''
+    Write an avida config series to avida config format
+    '''
+    cfg_series.to_csv(fp, sep=' ')
+
 
 # events: use json list of event lines, OR jinja template
 # avida.cfg: use json k:v pairs, join on default config
@@ -22,5 +38,10 @@ def parse_avida_cfg(filename):
 # analyze: should be static between experiments? (or jinja)
 # inst-set: static between experiments
 
-def update_avida_config(default_config, new_config_values):
-    assert type(default_config) is pd.Series
+def update_avida_cfg_series(cfg_series, new_config_values):
+    assert type(cfg_series) is pd.Series
+    for key, value in new_config_values.iteritems():
+        try:
+            cfg_series[key] = value
+        except IndexError as e:
+            print >>sys.stderr, 'Unable to update config key {} to value {}'.format(key, value)
